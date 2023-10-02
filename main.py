5ab3d355c16dc326c5271b8e055fa10461b7d4db
AndresBdull <andresbedullpl@gmail.com>
2023-10-02 10:45:20 -0500

    Creacion de la base de datos, funcionamineto de insercion

--- main.py
+++ main.py
@@ -2,52 +2,83 @@ import tkinter as tk
 from tkinter import ttk
 from tkinter import messagebox
 from tkinter import filedialog
+import sqlite3
 
 # Lista para almacenar las tarjetas creadas
 tarjetas = []
 
+## Base de datos
+def init_db():
+    conn = sqlite3.connect('Orquestador.db')
+
+    # Creación de la tabla "bots"
+    conn.execute('''
+    CREATE TABLE IF NOT EXISTS bots (
+        id INTEGER PRIMARY KEY,
+        name TEXT NOT NULL,
+        path TEXT NOT NULL,
+        status TEXT NOT NULL,
+        interval INTEGER NOT NULL,
+        l INTEGER NOT NULL,
+        m INTEGER NOT NULL,
+        x INTEGER NOT NULL,
+        j INTEGER NOT NULL,
+        v INTEGER NOT NULL,
+        last_run TEXT
+    );
+    ''')
+
+    cursor = conn.cursor()
+
+    return cursor
+# creacion de bot base
+def nuevo_bot(cursor,ventana_principal):
+
+    id = len(tarjetas) + 1
+
+    cursor.execute('''
+    INSERT INTO bots (name, path, status, interval, l, m, x, j, v, last_run)
+    VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+    ''', (f'Nuevo Bot {id}','c:/','active', 0, 0, 0, 0, 0, 0, "Sin ejecucion"))
+
+    cursor.connection.commit()
+
+    getAll(cursor,ventana_principal)
+# recuperar todo los datos
+def getAll(cursor, ventana_principal):
+    global tarjetas
+    cursor.execute('SELECT * FROM bots')
+    tarjetas = cursor.fetchall()
+
+    for bot in tarjetas:
+        boton_tarjeta = ttk.Button(ventana_principal, text=bot[1], command=lambda id=bot[0], nombre=bot[1]: abrir_tarjeta(cursor, id, nombre))
+        boton_tarjeta.pack(expand=True, fill="both")
+
+def actualizar_datos(cursor,id,nombre_tarjeta,campo_input,dias_semana,lista_dias,estado_var,campo_ruta_archivo):
+    intervalo = campo_input.get()
+    dias_seleccionados = [dias_semana[i] for i in lista_dias.curselection()]
+    estado = "Activo" if estado_var.get() else "Inactivo"
+    archivo_ruta = campo_ruta_archivo.get()  # Obtiene la ruta del archivo
+
+    # Imprimir datos en la consola, incluyendo la ruta del archivo
+    print(f"ID: {id}")
+    print(f"Nombre de la Tarjeta: {nombre_tarjeta}")
+    print(f"Intervalo (Minutos): {intervalo}")
+    print(f"Días seleccionados: {', '.join(dias_seleccionados)}")
+    print(f"Estado: {estado}")
+    print(f"Ruta del Archivo: {archivo_ruta}")
+
+    # Mostrar ventana emergente de confirmación
+    messagebox.showinfo("Confirmación", "Datos impresos en la consola.\n¡Confirmación exitosa!")
+
+
+## Frontend
 def seleccionar_archivo(campo_ruta_archivo):
     archivo_ruta = filedialog.askopenfilename()  # Abre el diálogo de selección de archivo
     campo_ruta_archivo.delete(0, tk.END)  # Borra el contenido actual del campo de entrada
     campo_ruta_archivo.insert(0, archivo_ruta)
 
-def abrir_ventana_input(id):
-    ventana_input = tk.Toplevel(ventana_principal)
-    ventana_input.title(f"Ventana #{id}")
-    
-    # Etiqueta e input en la nueva ventana
-    etiqueta = ttk.Label(ventana_input, text="Ingresa algo:")
-    etiqueta.pack(padx=10, pady=5)
-    
-    campo_input = ttk.Entry(ventana_input)
-    campo_input.pack(padx=10, pady=5)
-    
-    # Función para mostrar el contenido del campo de entrada
-    def mostrar_input():
-        contenido = campo_input.get()
-        messagebox.showinfo("Contenido", f"Input: {contenido} (ID: {id})")
-    
-    # Botón para mostrar el contenido del campo de entrada
-    boton_mostrar = ttk.Button(ventana_input, text="Mostrar Input", command=mostrar_input)
-    boton_mostrar.pack(padx=10, pady=10)
-
-def abrir_tarjeta(id, nombre_tarjeta):
-    def imprimir_datos():
-        intervalo = campo_input.get()
-        dias_seleccionados = [dias_semana[i] for i in lista_dias.curselection()]
-        estado = "Activo" if estado_var.get() else "Inactivo"
-        archivo_ruta = campo_ruta_archivo.get()  # Obtiene la ruta del archivo
-
-        # Imprimir datos en la consola, incluyendo la ruta del archivo
-        print(f"ID: {id}")
-        print(f"Nombre de la Tarjeta: {nombre_tarjeta}")
-        print(f"Intervalo (Minutos): {intervalo}")
-        print(f"Días seleccionados: {', '.join(dias_seleccionados)}")
-        print(f"Estado: {estado}")
-        print(f"Ruta del Archivo: {archivo_ruta}")
-
-        # Mostrar ventana emergente de confirmación
-        messagebox.showinfo("Confirmación", "Datos impresos en la consola.\n¡Confirmación exitosa!")
+def abrir_tarjeta(cursor, id, nombre_tarjeta):
 
     #Creacion de la Ventana
     ventana_tarjeta = tk.Toplevel(ventana_principal)
@@ -65,7 +96,7 @@ def abrir_tarjeta(id, nombre_tarjeta):
     campo_nombre.grid(row=1, column=0, padx=10, pady=5)
 
     #Ruta del .exe
-    etiqueta_archivo = ttk.Label(marco_tarjeta, text="Selecciona un archivo:")
+    etiqueta_archivo = ttk.Label(marco_tarjeta, text="Selecciona path archivo:")
     etiqueta_archivo.grid(row=2, column=0, padx=10, pady=5)
 
     campo_ruta_archivo = ttk.Entry(marco_tarjeta)
@@ -104,22 +135,29 @@ def abrir_tarjeta(id, nombre_tarjeta):
     checkbox_inactivo.grid(row=11, column=0, padx=10, pady=5, sticky="w")
 
     #Boton de recoleccion de datos
-    boton_imprimir = ttk.Button(marco_tarjeta, text="Imprimir Datos y Confirmar", command=imprimir_datos)
+    boton_imprimir = ttk.Button(marco_tarjeta, text="Imprimir Datos y Confirmar", command=lambda: actualizar_datos(cursor,id ,nombre_tarjeta, campo_input,dias_semana,lista_dias,estado_var,campo_ruta_archivo))
     boton_imprimir.grid(row=12, column=0, padx=10, pady=10)
 
-def agregar_tarjeta():
-    id_nueva_tarjeta = len(tarjetas) + 1
-    nombre_nueva_tarjeta = f"Tarjeta {id_nueva_tarjeta}"
-    tarjetas.append((id_nueva_tarjeta, nombre_nueva_tarjeta))
-    
-    boton_tarjeta = ttk.Button(ventana_principal, text=nombre_nueva_tarjeta, command=lambda id=id_nueva_tarjeta, nombre=nombre_nueva_tarjeta: abrir_tarjeta(id, nombre))
-    boton_tarjeta.pack(expand=True, fill="both")
 
+try:
+    #Inicilizo la base de datos
+    cursor = init_db()
+
+    # Creacion de la ventana principal
+    ventana_principal = tk.Tk()
+    ventana_principal.title("Orquestador")
+
+    #Creacion boton de agregar bot
+    boton_nueva_tarjeta = ttk.Button(ventana_principal, text="Crear Nuevo Bot",  command=lambda: nuevo_bot(cursor, ventana_principal))
+    boton_nueva_tarjeta.pack(expand=True, fill="both")
+
+    #REcupera todos los datos de la base de datos
+    getAll(cursor,ventana_principal)
+
+    # Agregar botón para crear una nueva tarjeta
+
+    ventana_principal.mainloop()
+
+except Exception as e:
+    print(e)
 # Crear la ventana principal
-ventana_principal = tk.Tk()
-ventana_principal.title("Orquestador")
-
-# Agregar botón para crear una nueva tarjeta
-boton_nueva_tarjeta = ttk.Button(ventana_principal, text="Crear Nueva Tarjeta", command=agregar_tarjeta)
-boton_nueva_tarjeta.pack(expand=True, fill="both")
-ventana_principal.mainloop()
